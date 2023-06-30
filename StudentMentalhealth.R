804b6077e2bdbde2a9b4bd97899ab64f1d1fbf94
Michelle Harris <shellhh01@gmail.com>
2023-06-30 11:39:09 -0400

    Since the two-way table shows us that there are at least 10 observations in each of the table's cells, we can safely continue this analysis using a theory-based approach.

--- StudentMentalhealth.R
+++ StudentMentalhealth.R
@@ -1,5 +1,11 @@
 #Michelle Harris
 #This is a short project that compares two proportions using a theory-based approach.
 
-attach(Student.Mental.health)
+library(dplyr)
+library(tidyr)
 
+View(Student.Mental.health)
+
+two_way_table <- Student.Mental.health %>%
+  count(Gender, Anxiety) %>%
+  tidyr::pivot_wider(names_from = Anxiety, values_from = n, names_prefix = "Anxiety_")
