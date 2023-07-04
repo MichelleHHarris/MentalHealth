c1c2d5dcdd37da6b6a2fbfc222a1d27c9d290b92
Michelle Harris <shellhh01@gmail.com>
2023-07-04 19:49:05 -0400

    I have added the other two validity conditions that we must check in order to complete this theory-based analysis.

--- StudentMentalhealth.R
+++ StudentMentalhealth.R
@@ -9,3 +9,12 @@ View(Student.Mental.health)
 two_way_table <- Student.Mental.health %>%
   count(Gender, Anxiety) %>%
   tidyr::pivot_wider(names_from = Anxiety, values_from = n, names_prefix = "Anxiety_")
+
+# We have determined that there are at least 10 observations in each of the cells, 
+# but now we must check the rest of the validity conditions. 
+
+# Samples must be selected at random, but since we aren't given that information for this data set,
+# we will assume that they were selected randomly.
+
+#Additionally, samples must be independent of each other, which this data set observes.
+
